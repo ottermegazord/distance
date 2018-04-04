59791930c051c55164203347cd411c217ff899bc
ottermegazord <idaly.ali@live.fr>
2018-04-04 10:32:37 -0400

    completes distance AL

--- exercise.js
+++ exercise.js
@@ -4,8 +4,14 @@ var exercise = {};
 exercise.distance = function(p1,p2){
     //-------------------
     //---- Your Code ----
-    //-------------------    
-    return 'Error: 1st function not implemented';    
+    //-------------------
+
+    var x_dist = (p1.x - p2.x);
+    var y_dist = (p1.y - p2.y);
+
+    var dist = Math.sqrt(Math.pow(y_dist, 2) + Math.pow(x_dist, 2));
+
+    return dist;
 };
 
 // calculate distance between new point and existing
@@ -13,15 +19,33 @@ exercise.distances = function(newPoint,data){
     //-------------------
     //---- Your Code ----
     //-------------------
-    return 'Error: 2nd function not implemented';
+
+    data.forEach(distancer);
+
+    function distancer(point){
+        point.distance = exercise.distance(newPoint, point);
+    }
+    return data;
 };
 
 // find k closest points
 exercise.findClosest = function(k,data){
     //-------------------
     //---- Your Code ----
-    //------------------- 
-    return 'Error: 3rd function not implemented';    
+    //-------------------
+
+    var closest = [];
+
+    data.sort(compare);
+
+    function compare(a, b){
+        return a.distance-b.distance;
+    }
+
+    for (var i = 0; i<k ; i++){
+        closest[i] = data[i];
+    }
+    return closest;
 };
 
 module.exports = exercise;
